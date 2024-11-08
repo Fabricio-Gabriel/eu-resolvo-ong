65f116bb26d4aa8034683f76123e6122df6de741
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-11-08 14:37:38 -0300

    implementando swiper js

--- frontend/index.js
+++ frontend/index.js
@@ -5,3 +5,36 @@ import 'regenerator-runtime/runtime';
 
 import './assets/css/style.css';
 
+console.log("inicializando swiper");
+const swiper = new Swiper(".swiper", {
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false
+    },
+    slidesPerView: 1,
+    loop: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+
+    navigation: {
+        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next"
+    },
+
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+        prev: {
+            shadow: true,
+            translate: ["-20%", 0, -1]
+        },
+
+        next: {
+            translate: ["101%", 0, 0]
+        }
+    }
+});
+
