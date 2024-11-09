78063c87b02d5d7ffba2c0e8f32bb61b1a7074a8
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-11-09 19:37:16 -0300

    navbar responsiva

--- frontend/index.js
+++ frontend/index.js
@@ -25,16 +25,9 @@ const swiper = new Swiper(".swiper", {
     },
 
     grabCursor: true,
-    effect: "creative",
-    creativeEffect: {
-        prev: {
-            shadow: true,
-            translate: ["-20%", 0, -1]
-        },
-
-        next: {
-            translate: ["100%", 0, 0]
-        }
+    effect: "fade", // Substitui o efeito criativo
+    fadeEffect: {
+        crossFade: true
     }
 });
 
